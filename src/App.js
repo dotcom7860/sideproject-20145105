081787b8cb66e6ca8f0d07681e13c7023196a11d
blackfourteen2014 <blackfourteen2014@gmail.com>
2020-09-08 19:02:57 +0900

    20/09/08

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
+
+class Content extends Component{
+  render() {
+    return (
+      <article>
+        <h3>HTML</h3>
+        HTML is ~~~
+      </article>
+    );
+  }
+}
+
+class TOC extends Component{
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="index.html">HTML</a></li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Subject extends Component{
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!!
+      </header>
+    );
+  }
+}
 
 class App extends Component { 
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }

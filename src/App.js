00cbf4101b4928162b9c281b8129c88085d86286
dotcom7860 <dotcom7860@gmail.com>
2020-09-15 15:44:18 +0900

    state 사용

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import Subject from "./components/Subject";
 import './App.css';
 
 class App extends Component { 
+  constructor(props){ //가장 먼저 실행되서 초기화를 담당
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );

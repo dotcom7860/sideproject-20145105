c74b0066e096319eeea006e11af8b3b1caa7ebdc
dotcom7860 <dotcom7860@gmail.com>
2020-09-15 16:59:32 +0900

    subject 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
   constructor(props){ //가장 먼저 실행되서 초기화를 담당
     super(props);
     this.state = {
+      mode:'read',
+      welcome:{title:'Welcome', desc:'Hello.React!!'},
       subject:{title:'WEB', sub:'World wide Web!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
@@ -16,12 +18,29 @@ class App extends Component {
       ]
     }
   }
-  render() {
+  render() { //props,state가 바뀌면 render()함수가 다시 불린다. 즉, 화면을 다시 그린다.
+    var _title, _desc = null;
+    if(this.state.mode=== 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            e.preventDefault();
+            this.setState({ //react에서 정해진 문법
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
